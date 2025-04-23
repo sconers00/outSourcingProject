a95559bd091bf95cb73f35b27349a04719202616
rlgkghkd <rlgkghkd@gmail.com>
2025-04-23 09:38:48 +0900

    feat: write prototype user entity

--- src/main/java/com/example/outsourcingproject/user/entity/User.java
+++ src/main/java/com/example/outsourcingproject/user/entity/User.java
@@ -1,5 +1,54 @@
 package com.example.outsourcingproject.user.entity;
 
-public class User {
+import com.example.outsourcingproject.user.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "user_table")
+@NoArgsConstructor
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @NotEmpty
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @NotNull
+    @ColumnDefault("false")
+    private boolean isDeleted;
+
+    @Builder
+    public User(String email, String password, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
 }
---
+++ src/main/java/com/example/outsourcingproject/user/enums/UserRole.java
@@ -0,0 +1,14 @@
+package com.example.outsourcingproject.user.enums;
+
+import java.util.Arrays;
+
+public enum UserRole {
+    OWNER, CUSTOMER, ADMIN;
+
+    public static UserRole of(String role){
+        return Arrays.stream(UserRole.values())
+                .filter(r-> r.name().equalsIgnoreCase(role))
+                .findFirst()
+                .orElseThrow();
+    }
+}
