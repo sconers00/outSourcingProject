7fe32279bd81037ffc54b7f7119b02d8b2b39be2
rlgkghkd <rlgkghkd@gmail.com>
2025-04-24 17:39:30 +0900

    fix: searching orders by jpql

--- src/main/java/com/example/outsourcingproject/order/repository/OrderRepository.java
+++ src/main/java/com/example/outsourcingproject/order/repository/OrderRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -13,7 +15,8 @@ import com.example.outsourcingproject.order.entity.Orders;
 import com.example.outsourcingproject.user.entity.User;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
-	Optional<List<Orders>> findAllByUser(User user);
+	@Query("SELECT o FROM Orders o where o.user = :user")
+	Optional<List<Orders>> findAllByUser(@Param("user") User user);
 
 	default PageImpl<Orders> findAllByUserOrElseThrow(User user, PageRequest pageRequest) {
 		List<Orders> foundedList = findAllByUser(user).orElseThrow(
