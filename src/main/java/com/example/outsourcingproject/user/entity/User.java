d900a86f82915f4cf6f843bb2c958cefeba5557d
rlgkghkd <rlgkghkd@gmail.com>
2025-04-25 10:49:26 +0900

    fix: add annotation to user entity's email

--- src/main/java/com/example/outsourcingproject/user/entity/User.java
+++ src/main/java/com/example/outsourcingproject/user/entity/User.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -32,6 +33,7 @@ public class User extends BaseEntity {
 	private Long userId;
 
 	@NotEmpty
+	@Email
 	@Column(unique = true)
 	private String email;
 
